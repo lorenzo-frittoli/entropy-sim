9d1d3bcc4f65198fc6d7edc47342af1e9313d1cf
lorenzo-frittoli <lorenzo.frittoli.ge@gmail.com>
2023-10-24 16:43:52 +0200

    Added docstrings
    Added docstrings to Ensamble's __init__() and calculate_entropy(), to
    Element's __init()__.

--- element.py
+++ element.py
@@ -1,5 +1,12 @@
 class Element:
     def __init__(self, state: int, position: int, states_distribution: list[float] = [0.5, 0.5]) -> None:
+        """Class that represents an element of a system
+
+        Args:
+            state (int): State of the element
+            position (int): Position of the element (currently unused)
+            states_distribution (list[float], optional): Probability distribution of the states. Defaults to [0.5, 0.5].
+        """
         self.state = state
         self.position = position
         self.states_distribution = states_distribution
--- ensamble.py
+++ ensamble.py
@@ -7,11 +7,22 @@ from element import Element
 
 class Ensamble:
     def __init__(self, length: int) -> None:
+        """Ensable class that represents a system of Element objects.
+
+        Args:
+            length (int): _description_
+        """
         self.elements = [Element(random.randint(0, 1), i, [0.2, 0.8]) for i in range(length)]
         
     def __str__(self) -> str:
         return ", ".join([str(e) for e in self.elements])
     
-    def calculate_entropy(self):
+    def calculate_entropy(self) -> float:
+        """Calculates the entropy of the system based on its elements' variables
+
+        Returns:
+            float: entropy of the system
+        """
         probabilities = [prod(c) for c in product(*[e.states_distribution for e in self.elements])]
         return -sum([p * log2(p) for p in probabilities])
+    
